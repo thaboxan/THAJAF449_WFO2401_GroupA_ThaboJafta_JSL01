9cc87ebdd53db3aa21cea5ebd121732111ccf70d
ThaboCodes <thvbojafta@gmail.com>
2024-03-15 09:44:16 +0200

    refined code to use a ternary operator instead of a conditional

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 function validateSyntax() {
   let input = document.getElementById("petInput").value;
-  let result = "";
   const regex = /^pet_\d{4}[^\d_]+$/; // RegEx to validate the input
 
-  // Validation logic
-  if (regex.test(input)) {
-    result = "Valid Syntax";
-  } else {
-    result = "Invalid Syntax";
-  }
+  // Validation logic using ternary operator
+  const result = regex.test(input) ? "Valid Syntax" : "Invalid Syntax";
 
   document.getElementById("result").innerText = result;
 }
